6a235b517f99316b23b0a3437fbf4dab29064a27
alonsocj <cornejo.aj98@hotmail.com>
2021-06-21 18:21:34 -0600

    se establecieron directorios para las templates

--- SistemaControlDeVacunas/BackendApp/urls.py
+++ SistemaControlDeVacunas/BackendApp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from Import_Csv import urls
+from .views import HomePage
 
 urlpatterns = [
-    path('importarCsv/', include('Import_Csv.urls'))
+    path('importarCsv/', include('Import_Csv.urls')),
+    path('', HomePage.as_view(), name = 'Home'),
 ]
