57758eaea46e621c5d3032e5164be76db522419a
alonsocj <cornejo.aj98@hotmail.com>
2021-06-21 22:25:44 -0600

    se agrego el importar personas por medio de csv y graficos (no funcionales)

--- SistemaControlDeVacunas/SistemaControlDeVacunas/urls.py
+++ SistemaControlDeVacunas/SistemaControlDeVacunas/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('BackendApp.urls'))
+    path('', include('BackendApp.urls')),
+    path('import/', include('Import_Csv.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
