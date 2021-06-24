394f1d5c8b80a8e36b4a6491849bbf5c2ee2d376
alonsocj <cornejo.aj98@hotmail.com>
2021-06-23 23:58:07 -0600

    se resolvieron unas excepciones a la hora de importar datos y se evita la duplicaion de registros

--- SistemaControlDeVacunas/Import_Csv/views.py
+++ SistemaControlDeVacunas/Import_Csv/views.py
@@ -28,7 +28,7 @@ class DatosPersona(TemplateView):
             form = CsvModelForm()
             obj = Csv.objects.get(activated=False)
             with open(obj.file_name.path, 'r') as f:
-                per=0
+                per = 0
                 regis = 0
                 reader = csv.reader(f)
                 for i, row in enumerate(reader):
@@ -42,8 +42,6 @@ class DatosPersona(TemplateView):
                     dosis = row[7]
                     fecha = row[8]
 
-                    
-
                     try:
                         Persona.objects.create(
                             dui=dui,
@@ -54,12 +52,28 @@ class DatosPersona(TemplateView):
                             sexo=sexo,
                             edad=int(edad)
                         )
-                    except :
+                    except:
                         #print('La Persona con el dui: '+str(dui)+', ya existe en la tabla de Personas')
-                        per+=1
+                        per += 1
+                    ban = False
                     try:
+                        Registro.objects.filter(dui=dui)[0]
+                    except:
+                        ban = True
+
+                    if ban:
+                        Registro.objects.create(
+                                dui=Persona.objects.get(dui=dui),
+                                nombre_vacuna=TipoVacuna.objects.get(
+                                    nombre_vacuna=vacuna),
+                                numero_dosis=Dosis.objects.get(numero_dosis=dosis),
+                                fecha_vacunacion=fecha
+                            )
+                        pass
+                    else:
                         if dui == str(Registro.objects.filter(dui=dui)[0]):
-                            print('La Persona con el dui: '+str(dui)+', ya existe en la tabla de Registros')
+                            print('La Persona con el dui: '+str(dui) +
+                                ', ya existe en la tabla de Registros')
                         else:
                             Registro.objects.create(
                                 dui=Persona.objects.get(dui=dui),
@@ -68,8 +82,6 @@ class DatosPersona(TemplateView):
                                 numero_dosis=Dosis.objects.get(numero_dosis=dosis),
                                 fecha_vacunacion=fecha
                             )
-                    except :
-                        print(dui+', no existe')
                 obj.activated = True
                 obj.save()
         return HttpResponseRedirect(self.success_url)
