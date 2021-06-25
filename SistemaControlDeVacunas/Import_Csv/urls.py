609c75c9abadcd640d42916b51e21cbe2618eced
CarolinaIPD <delgadoisabel571@gmail.com>
2021-06-24 22:27:27 -0600

    Cambios en pantalla inicial

--- SistemaControlDeVacunas/Import_Csv/urls.py
+++ SistemaControlDeVacunas/Import_Csv/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from .views import DatosPersona
 
 from BackendApp.views import HomePage
@@ -7,6 +8,6 @@ app_name = 'csvs'
 
 urlpatterns=[
     path('', HomePage.as_view(), name='Home'),
-    path('csv/',DatosPersona.as_view(), name='upload-view'),
+    path('csv/',login_required(DatosPersona.as_view()), name='upload-view'),
 
 ]
