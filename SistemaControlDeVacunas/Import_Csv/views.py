4509d077e64abdce4ff8d91b8bd1dc00c1274585
alonsocj <cornejo.aj98@hotmail.com>
2021-06-25 00:03:38 -0600

    se agregaron excepciones al no existir una vacuna al momento de importar datos

--- SistemaControlDeVacunas/Import_Csv/views.py
+++ SistemaControlDeVacunas/Import_Csv/views.py
@@ -30,6 +30,7 @@ class DatosPersona(TemplateView):
             with open(obj.file_name.path, 'r') as f:
                 per = 0
                 regis = 0
+                vacum = 0
                 reader = csv.reader(f)
                 for i, row in enumerate(reader):
                     dui = row[0]
@@ -62,16 +63,18 @@ class DatosPersona(TemplateView):
                         ban = True
 
                     if ban:
-                        Registro.objects.create(
-                            dui=Persona.objects.get(dui=dui),
-                            nombre_vacuna=TipoVacuna.objects.get(
-                                nombre_vacuna=vacuna),
-                            numero_dosis=Dosis.objects.get(numero_dosis=dosis),
-                            fecha_vacunacion=fecha
-                        )
-                        pass
+                        try:
+                            Registro.objects.create(
+                                dui=Persona.objects.get(dui=dui),
+                                nombre_vacuna=TipoVacuna.objects.get(
+                                    nombre_vacuna=vacuna),
+                                numero_dosis=Dosis.objects.get(numero_dosis=dosis),
+                                fecha_vacunacion=fecha
+                            )
+                        except :
+                            vacum +=1
                     else:
-                        if dui == str(Registro.objects.filter(dui=dui)[0]):
+                        if dui == str(Registro.objects.filter(dui=dui)[0] and dosis == 1):
                             messages.warning(request, 'La Persona con el dui: '+str(dui) +
                                              ', ya existe en la tabla de Registros\n')
                             regis +=1
@@ -86,8 +89,11 @@ class DatosPersona(TemplateView):
                             )
             obj.activated = True
             obj.save()
-            if per == 0 or regis ==0: 
+            if per == 0 or regis ==0 and vacum ==0: 
                 messages.success(request,'Datos ingresados con exito')
+            elif vacum > 0 :
+                messages.warning(request,"Es posible que no se encuentre en la base de datos la vacuna asignada en un registro")
+
             return render(request, self.template_name, {'form': form})
 
     def get_context_data(self, **kwargs):
